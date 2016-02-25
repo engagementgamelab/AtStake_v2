7bf1217a2f9280b4e8ef622b6c1104b2ab24e66c
jayvachon <jaylvachon@gmail.com>
2016-02-25 10:35:31 -0500

    set up basic templates for all views

--- Assets/Scripts/Screen/Views/AgendaItemReject.cs
+++ Assets/Scripts/Screen/Views/AgendaItemReject.cs
@@ -7,14 +7,14 @@ namespace Views {
 		
 		protected override void OnInitPlayerElements () {
 			if (MyItem) {
-				Elements.Add ("your_item", new TextElement (
+				Elements.Add ("agenda_item", new TextElement (
 					DataManager.GetTextFromScreen (Model, "your_item", RewardTextVariables)));
 			}
 		}
 
 		protected override void OnInitElements () {
 			if (!MyItem) {
-				Elements.Add ("their_item", new TextElement (
+				Elements.Add ("agenda_item", new TextElement (
 					DataManager.GetTextFromScreen (Model, "their_item", RewardTextVariables)));
 			}		
 		}
