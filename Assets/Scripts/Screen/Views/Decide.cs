ec765f0f863e918e6a7706765454041d5b6c0acd
jayvachon <jaylvachon@gmail.com>
2016-04-25 12:36:15 -0400

    update decide screen to use radio list

--- Assets/Scripts/Screen/Views/Decide.cs
+++ Assets/Scripts/Screen/Views/Decide.cs
@@ -6,25 +6,11 @@ using System.Collections.Generic;
 namespace Views {
 
 	public class Decide : View {
-
-		string selectedPeer;
-
+		
 		protected override void OnInitDeciderElements () {
-
-			// Confirmation button. Can only be pressed after a player has been selected.
-			Elements.Add ("confirm", new ButtonElement ("Confirm", () => {
-				Game.Dispatcher.ScheduleMessage ("ChooseWinner", selectedPeer);
-			}) { Interactable = false });
-
-			// A button list of players
-			Dictionary<string, ButtonElement> peers = Game.Controller.PeerNames.ToDictionary (x => x, x => new ButtonElement (x, () => {
-				selectedPeer = x;
-				ButtonElement confirmButton = GetScreenElement<ButtonElement> ("confirm");
-				confirmButton.Text = "Confirm " + selectedPeer;
-				confirmButton.Interactable = true;
-			}));
-
-			Elements.Add ("peer_list", new ListElement<ButtonElement> (peers));
+			Elements.Add ("peer_list", new RadioListElement (GetButton ("confirm"), (string selected) => {
+				Game.Dispatcher.ScheduleMessage ("ChooseWinner", selected);
+			}, Game.Controller.PeerNames));
 		}
 
 		protected override void OnShow () { Game.Dispatcher.AddListener ("ChooseWinner", ChooseWinner); }
